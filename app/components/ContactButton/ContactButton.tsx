0b96c29623a9fc332e17c9a506485353972e9b3b
stacey.sych@global.com <stacey.sych@global.com>
2024-08-02 19:30:06 +0100

    test

--- app/components/ContactButton/ContactButton.tsx
+++ app/components/ContactButton/ContactButton.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@chakra-ui/react";
 import { useLocale } from "@/app/providers";
 import NextLink from "next/link";
-import { BASE_URL } from "@/app/utils/defaults";
 
 const ContactButton = () => {
   const {
@@ -17,7 +16,7 @@ const ContactButton = () => {
       bg={"orange.main"}
       _hover={{ bg: "orange.dark" }}
     >
-      <NextLink href={`${BASE_URL}#${link}`}>{title}</NextLink>
+      <NextLink href={`#${link}`}>{title}</NextLink>
     </Button>
   );
 };
