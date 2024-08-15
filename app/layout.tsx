255ace1c6abf08f2f4bdf934c7d9d4f067e76836
stacey.sych@global.com <stacey.sych@global.com>
2024-08-15 14:47:38 +0100

    chore: add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider, LocaleProvider } from "./providers";
 import { Metadata } from "next";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 export const metadata: Metadata = {
   title: {
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GoogleAnalytics />
       <body>
         <ChakraProvider>
           <LocaleProvider>
